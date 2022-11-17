942040ff6959c2e0a88236b8386a2e28383a99ce
alex <aabinaei@gmail.com>
2022-11-16 17:31:32 -0800

    Code TLC.

--- web/js/anchor/config.ts
+++ web/js/anchor/config.ts
@@ -1,11 +1,7 @@
-import {clusterApiUrl, PublicKey} from "@solana/web3.js";
+import {PublicKey} from "@solana/web3.js";
+import {RPC_URL} from "./rpc";
 
 
 export const COMMITMENT = "processed";
 export const PROGRAM_ID = new PublicKey("Gqe2dtQYS3GBzyrDaMmHE4JcJAW7cFhiBsNQpaSGtzFV");
-
-// const localnet = "http://127.0.0.1:8899";
-// const devnet = clusterApiUrl("devnet");
-// const mainnet = clusterApiUrl("mainnet-beta");
-const mainnet = "https://solana-api.projectserum.com"
-export const NETWORK = mainnet;
+export const NETWORK = RPC_URL;
