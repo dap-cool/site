036b70aca0460f50737c0a6a107f8d70430bbd0b
alex <aabinaei@gmail.com>
2022-12-26 10:02:10 -0800

    bumped cloudfront id

--- publish.sh
+++ publish.sh
@@ -18,8 +18,7 @@ aws s3 sync web/images/ $BUCKET/images/ --profile tap-in
 aws s3 cp web/index.html $BUCKET --profile tap-in
 aws s3 cp web/elm.min.js $BUCKET --profile tap-in
 aws s3 cp web/css/ $BUCKET/css/ --recursive --profile tap-in
-aws s3 cp web/fonts/ $BUCKET/fonts/ --recursive --profile tap-in
 aws s3 cp web/js/bundle.js $BUCKET/js/ --profile tap-in
 
 echo "Invalidating CloudFront Cache..."
-aws cloudfront create-invalidation --distribution-id E23ECDNPH9RTM7 --paths "/*" --profile tap-in
+aws cloudfront create-invalidation --distribution-id EL3LYJT51ADO1 --paths "/*" --profile tap-in
